6a5b6a5f1f8fe12f3b490b1e3b6651bd51b75975
anmolshetty02 <shetty.anm@northeastern.edu>
2025-04-18 17:00:34 -0400

    Fix: Update CORS to support deployed frontend

--- ProLance/backend/server.js
+++ ProLance/backend/server.js
@@ -3,5 +3,7 @@ import initialize from './apps/app.js';
 
 const app = express();
 const port = 3000;
+
+app.use('/upload', express.static('upload'));
 initialize(app);
 app.listen(port, () => console.log('Server is listening at port 3000'));
